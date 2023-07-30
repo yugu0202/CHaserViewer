c1e8544f200945added7ea8e1558492f087ea506
yugu-0202 <applerin1119@gmail.com>
2023-07-31 04:38:02 +0900

    view description

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ document.onkeyup = function (e) {
 }
 
 reader.onload = () => {
-	console.log(reader.result.split(/\n/));
-	log = reader.result.split(/\n/);
+	// splitで改行区切りにし、filterで空行を無視している
+	log = reader.result.split(/\r\n/).filter(v => v);
 	View();
 }
 
@@ -36,6 +36,7 @@ function View() {
 	function MapView()
 	{
 		var scene = new Scene();
+
 		for (let y=0; y < size_y; y++) {
 			var line = log[pointer+y].split(/,/).map( str => parseInt(str, 10));
 
@@ -60,36 +61,46 @@ function View() {
 			}
 		}
 
-		pointer = pointer + size_y;
+		pointer += size_y;
 
-		cool = new Sprite(32, 32);
-		cool.image = core.assets['img/Cool.png'];
 		var [ cool_x, cool_y ] = log[pointer].split(/,/).map( str => parseInt(str, 10));
-		console.log(cool_x);
-		console.log(cool_y);
-		cool.x = cool_x*32;
-		cool.y = cool_y*32;
-		scene.addChild(cool);
 		pointer++;
 
-		hot = new Sprite(32, 32);
-		hot.image = core.assets['img/Hot.png'];
 		var [ hot_x, hot_y ] = log[pointer].split(/,/).map( str => parseInt(str, 10));
-		console.log(hot_x);
-		console.log(hot_y);
-		hot.x = hot_x*32;
-		hot.y = hot_y*32;
-		scene.addChild(hot);
 		pointer++;
 
-		if (pointer == size_y + 2)
+		[ cool_item, hot_item ] = log[pointer].split(/,/).map( str => parseInt(str,10));
+
+		if (log[pointer + 1] == "gameend")
 		{
-			[ cool_name, hot_name ] = log[pointer].split(/,/);
+			end_caution = "game end !!";
 		}
 		else
 		{
-			[ cool_item, hot_item ] = log[pointer].split(/,/).map( str => parseInt(str,10));
+			end_caution = "";
 		}
+
+		var test_data = [`map: ${map_name}`,`remain turn: ${turn}`,`cool: ${cool_name} item: ${cool_item}`,`hot: ${hot_name} item ${hot_item}`,`result: ${result_text}`,reason,end_caution];
+
+		test_data.forEach((elem, index) => {
+			var label = new Label(elem);
+			label.font = "24px Palatino";
+			label.x = size_x*32 + 10;
+			label.y = 20*index+10;
+			scene.addChild(label);
+		})
+
+		cool = new Sprite(32, 32);
+		cool.image = core.assets['img/Cool.png'];
+		cool.x = cool_x*32;
+		cool.y = cool_y*32;
+		scene.addChild(cool);
+
+		hot = new Sprite(32, 32);
+		hot.image = core.assets['img/Hot.png'];
+		hot.x = hot_x*32;
+		hot.y = hot_y*32;
+		scene.addChild(hot);
 
 		scene.addEventListener('enterframe', function() 
 		{
@@ -101,11 +112,21 @@ function View() {
 			{
 				right_count = 0;
 			}
-			if(core.input.right && right_count%10 == 0 && log[pointer+1] !== 'gameend')
+			if(core.input.right && right_count%10 == 0)
 			{
-				pointer++;
+				if (log[pointer+1] == "gameend")
+				{
+					end_caution = "game end!!";
+					pointer -= size_y + 2;
+				}
+				else
+				{
+					end_caution = "";
+					turn--;
+					pointer++;
+				}
+
 				nexted_flag = true;
-				console.log(log[pointer]);
 				MapView();
 			}
 			if(core.input.left)
@@ -118,35 +139,47 @@ function View() {
 			}
 			if(core.input.left && left_count%10 == 0 && pointer >= 39)
 			{
-				pointer -= 39;
+				end_caution = "";
+				turn++;
+				pointer -= size_y*2 + 5;
 				preved_flag = true;
-				console.log(log[pointer]);
 				MapView();
 			}
 		})
 
-		console.log(pointer);
 		core.replaceScene(scene);
 	}
 
 	var pointer = 0;
-	var cool_name;
-	var hot_name;
-	var cool_item;
-	var hot_item;
-	var map_name = log[0];
-	turn = log[1];
-	var [ size_x, size_y ] = log[2].split(/,/).map( str => parseInt(str, 10));
+	var result_text, end_caution;
+	var cool_item = 0;
+	var hot_item = 0;
+	var [ cool_name, hot_name ] = log[0].split(/,/);
+	var map_name = log[1];
+	turn = log[2];
+	var [ size_x, size_y ] = log[3].split(/,/).map( str => parseInt(str, 10));
+	var [ winner, result, reason ]  = log.pop().split(/,/);
+	if (winner)
+	{
+		result_text = `${winner} win`;
+	}
+	else
+	{
+		result_text = result;
+	}
 
-	console.log(map_name);
-	console.log(turn);
-	console.log(size_x);
-	console.log(size_y);
+	console.log(`map name: ${map_name}`);
+	console.log(`turn: ${turn}`);
+	console.log(`map size x: ${size_x}`);
+	console.log(`map size y: ${size_y}`);
+	console.log(`cool name: ${cool_name}`);
+	console.log(`hot name: ${hot_name}`);
+	console.log(`result: ${result_text}`);
 
-	var core = new Core(672, 672);
+	var core = new Core(960, 960);
 	core.preload(['img/None.png','img/Block.png','img/Item.png','img/Cool.png','img/Hot.png']);
 	core.onload = function() {
-		log = log.slice(3);
+		log = log.slice(4);
 		MapView(0);
 	};
 
